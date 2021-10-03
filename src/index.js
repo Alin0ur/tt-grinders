845c4e54be182adc2feaf3ac34cb3ce34b1a72b6
Alin0ur <68285125+Alin0ur@users.noreply.github.com>
2021-10-03 23:32:10 +0200

    added imports

--- src/index.js
+++ src/index.js
@@ -15,15 +15,24 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-/*
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import AdminLayout from "layouts/Admin/Admin.js";
-*/
+
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+            <Redirect from="/" to="/admin/dashboard" />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
