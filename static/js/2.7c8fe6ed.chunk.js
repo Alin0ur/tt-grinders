b0f54db5e544e9e687f6a16692ad0a6d7354d7c6
Alin0ur <68285125+Alin0ur@users.noreply.github.com>
2021-10-03 23:10:24 +0200

    Updates

---
+++ static/js/2.7c8fe6ed.chunk.js
@@ -0,0 +1,19 @@
+/*!
+
+=========================================================
+* Black Dashboard React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+(this["webpackJsonpblack-dashboard-react"]=this["webpackJsonpblack-dashboard-react"]||[]).push([[2],{4:function(a,c,s){}}]);
+//# sourceMappingURL=2.7c8fe6ed.chunk.js.map
--- static/js/main.498b630b.chunk.js
+++ static/js/main.498b630b.chunk.js
@@ -1,7 +1,7 @@
 /*!
 
 =========================================================
-* Black Dashboard React v1.2.0
+* Black Dashboard React - v1.2.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/black-dashboard-react
@@ -15,3 +15,5 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+(this["webpackJsonpblack-dashboard-react"]=this["webpackJsonpblack-dashboard-react"]||[]).push([[0],[function(c,a,n){"use strict";n.r(a);n(1),n(2),n(3),n(4)},function(c,a,n){},function(c,a,n){},function(c,a,n){}],[[0,1,2]]]);
+//# sourceMappingURL=main.498b630b.chunk.js.map
